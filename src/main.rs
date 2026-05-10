01dc78282c0979837715d63b3b61103e6ce12501
michael-0acf4 <afmichael73@gmail.com>
2026-05-10 18:10:34 +0300

    First COmmit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use tracing_subscriber::EnvFilter;
+
+fn main() {
+    let env_filter = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("brutefs=DEBUG"))
+        .unwrap();
+    tracing_subscriber::fmt()
+        .with_env_filter(env_filter)
+        .without_time() 
+        .init();
+
+    tracing::debug!("Hello world");
+}
